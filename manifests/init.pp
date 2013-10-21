15db42f0fd6bc33c4df278cd10787275098755e5
Christopher Petersen <chris@petersen.io>
2013-10-21 10:22:24 -0700

    Initial version

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class gephi {
+  package { 'gephi':
+    provider => 'appdmg',
+    source   => 'https://launchpad.net/gephi/0.8/0.8.2beta/+download/gephi-0.8.2-beta.dmg'
+  }
 }
